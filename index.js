c4604f2f8e0a2b4204c2bd0e6e8efd6900b73e74
John <johnmulvey48@yahoo.com>
2021-12-16 10:14:04 -0500

    git@github.com:Jmulvez/phase-1-first-class-functions.git

---
+++ index.js
@@ -0,0 +1,12 @@
+let eatBreakfast = Cereal
+receivesAFunction(function (eatBreakfast)) {
+    console.log('morningRoutine');
+}
+let morningWorkout = fiveMileRun
+returnsANamedFunction(function(morningWorkout)) {
+    console.log('morningWorkout');
+}
+
+returnsAnAnonymousfunction(function()); {
+    console.log(function());
+}
